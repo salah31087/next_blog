95de52586ad53291ebc9a94646db726dc24f02dc
salah eddine <salahbelaid3108@gmail.com>
2025-05-17 22:53:34 +0100

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -37,13 +37,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XZVDY3JF2R"></Script>
-        <Script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-XZVDY3JF2R');
+        {/* Google Analytics script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XZVDY3JF2R"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XZVDY3JF2R');
+          `}
         </Script>
       </head>
       <body
